9afc81676c3486953fcbe097d18132c42c6308dc
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2024-12-13 15:06:26 +0700

    update

--- sts-jurysystem/app/properties/[id]/page.jsx
+++ sts-jurysystem/app/properties/[id]/page.jsx
@@ -1,10 +1,27 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link"
 
-const PropertyPage = ({params, searchParams}) => {
+const PropertyPage = async ({params}) => {
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
 
-    return <div>
-        <div>{searchParams.name}</div>
-        Property Page { params.id }
-    </div>;
+    return 
+    <>
+    <PropertyHeaderImage image={property.images[0]}/>
+    <section>
+      <div className="container m-auto py-6 px-6">
+        <Link
+          href="/properties"
+          className="text-blue-500 hover:text-blue-600 flex items-center"
+        >
+          <i className="fas fa-arrow-left mr-2"></i> Back to Properties
+        </Link>
+      </div>
+    </section>
+    <section> { property.name }</section>;
+    </>
 }
  
 export default PropertyPage;
