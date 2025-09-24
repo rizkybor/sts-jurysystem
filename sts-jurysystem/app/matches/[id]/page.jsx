7b9e5d72dc6c0d7655e39517f53fe700c929fcb3
ariyandi subekti <bektiariyandi@gmail.com>
2025-09-24 18:06:23 +0700

    update matches

--- sts-jurysystem/app/matches/[id]/page.jsx
+++ sts-jurysystem/app/matches/[id]/page.jsx
@@ -2,7 +2,6 @@
 import { useParams } from 'next/navigation'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-
 const DEFAULT_IMG = '/images/logo-dummy.png'
 
 export default function EventDetailPage() {
